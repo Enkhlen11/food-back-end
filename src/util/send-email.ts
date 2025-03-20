b117f33fbc8f0755d8f7f50894217947553f82d6
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-03-20 12:07:26 +0800

    j

--- src/util/send-email.ts
+++ src/util/send-email.ts
@@ -1,2 +1,24 @@
 import nodemailer from "nodemailer";
-export const sendEmail = async (email: string, token: string) => {};
+
+export const sendEmail = async (email: string, token: string) => {
+  const mailSecret = process.env.MAIL_SECRET;
+  const mail = process.env.MAIL;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465, //port
+    secure: true, //this is true as port id 465
+    auth: {
+      user: mail,
+      pass: mailSecret,
+    },
+  });
+
+  const mailOption = {
+    from: "Nom nom foods",
+    to: email,
+    subject: "Reset your password",
+    html: `<h1>Your password reser link</h1>
+  <a href="http://localhost:3000/reset-password?id=${token}">click here</a>`,
+  };
+  await transporter.sendMail(mailOption);
+};
