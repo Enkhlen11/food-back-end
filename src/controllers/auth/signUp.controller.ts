1e148ce1ae872f025b850c679cb8afa8e457abb9
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-03-21 09:52:39 +0800

    k

--- src/controllers/auth/signUp.controller.ts
+++ src/controllers/auth/signUp.controller.ts
@@ -1,1 +1,10 @@
-exoprt const 
+import { Request, Response } from "express";
+import userModel from "../../models/user.model";
+
+export const signUp = (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  userModel.create({ email, password });
+  if (!email) {
+    res.status(401).json({ message: "Emailaa bichne uu" });
+  }
+};
